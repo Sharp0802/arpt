1782016b8cc3557cdcb1cae606ab6143cdf0d65a
Sharp0802 <sharp0802.business@gmail.com>
2024-05-01 22:49:50 +0900

    Chore: Prepare android toolchain

---
+++ cmake/Android.aarch64.cmake
@@ -0,0 +1,15 @@
+
+execute_process(
+    COMMAND $ENV{SHELL} -c "dirname \"$(which ndk-build))\""
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    OUTPUT_VARIABLE NDK_HOME)
+
+message(STATUS "NDK found in '" ${NDK_HOME} "'")
+
+set(CMAKE_ANDROID_NDK ${NDK_HOME})
+
+set(CMAKE_SYSTEM_NAME       "Android")
+set(CMAKE_SYSTEM_VERSION    24)
+set(CMAKE_ANDROID_ARCH_ABI  "arm64-v8a")
+set(CMAKE_ANDROID_STL_TYPE  "c++_static")
+set(CMAKE_ANDROID_TOOLCHAIN "clang")
