2bf95857406d8865052667e8dcd9ba127d1c298c
Sharp0802 <sharp0802.business@gmail.com>
2024-05-04 22:14:54 +0900

    Feat: Create IPv6 structures

--- inc/ip.h
+++ inc/ip.h
@@ -28,25 +28,52 @@ namespace arpt
         std::string ToString() const;
     };
 
+    template<>
+    class IPImpl<6>
+    {
+        std::array<uint8_t, 16> m_Data;
+
+    public:
+        explicit IPImpl(const uint8_t* data);
+
+        explicit IPImpl(std::array<uint8_t, 16> data);
+
+        [[nodiscard]]
+        operator std::array<uint8_t, 16>() const;
+
+        [[nodiscard]]
+        operator const uint8_t*() const;
+
+        [[nodiscard]]
+        std::string ToString() const;
+    };
+
     class IP
     {
         std::uint8_t m_Version;
         IPImpl<4> m_4;
+        IPImpl<6> m_6;
 
     public:
         IP(const uint8_t* data, uint8_t version);
 
-        explicit IP(std::array<uint8_t, 16> data); // not ready
+        explicit IP(std::array<uint8_t, 16> data);
         explicit IP(std::array<uint8_t, 4> data);
 
         [[nodiscard]]
         IPImpl<4> __GetIPv4() const;
 
         [[nodiscard]]
+        IPImpl<6> __GetIPv6() const;
+
+        [[nodiscard]]
         uint8_t __GetVersion() const;
 
         __declspec(property(get=__GetIPv4))
         IPImpl<4> IPv4;
+
+        __declspec(property(get=__GetIPv6))
+        IPImpl<4> IPv6;
 
         __declspec(property(get=__GetVersion))
         uint8_t Version;
--- src/ip.cxx
+++ src/ip.cxx
@@ -14,12 +14,12 @@ namespace arpt
     {
     }
 
-    IPImpl<4>::operator std::array<unsigned char, 4>() const
+    IPImpl<4>::operator std::array<uint8_t, 4>() const
     {
         return m_Data;
     }
 
-    IPImpl<4>::operator const unsigned char*() const
+    IPImpl<4>::operator const uint8_t*() const
     {
         return m_Data.data();
     }
@@ -29,19 +29,55 @@ namespace arpt
         return std::format("{}.{}.{}.{}", m_Data[0], m_Data[1], m_Data[2], m_Data[3]);
     }
 
+    IPImpl<6>::IPImpl(const uint8_t* data)
+        : m_Data{
+            data[0], data[1], data[2], data[3],
+            data[4], data[5], data[6], data[7],
+            data[8], data[9], data[10], data[11],
+            data[12], data[13], data[14], data[15],
+        }
+    {
+    }
+
+    IPImpl<6>::IPImpl(std::array<uint8_t, 16> data) : m_Data(data)
+    {
+    }
+
+    IPImpl<6>::operator std::array<uint8_t, 16>() const
+    {
+        return m_Data;
+    }
+
+    IPImpl<6>::operator const uint8_t*() const
+    {
+        return m_Data.data();
+    }
+
+    std::string IPImpl<6>::ToString() const
+    {
+        return std::format(
+            "{:02X}{:02X}:{:02X}{:02X}:{:02X}{:02X}:{:02X}{:02X}:"
+            "{:02X}{:02X}:{:02X}{:02X}:{:02X}{:02X}:{:02X}{:02X}",
+            m_Data[0], m_Data[1], m_Data[2], m_Data[3],
+            m_Data[4], m_Data[5], m_Data[6], m_Data[7],
+            m_Data[8], m_Data[9], m_Data[10], m_Data[11],
+            m_Data[12], m_Data[13], m_Data[14], m_Data[15]);
+    }
+
     IP::IP(const uint8_t* data, uint8_t version)
-        : m_4(data)
+        : m_4({ 0, 0, 0, 0 }),
+          m_6({ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 })
     {
         switch (version)
         {
         case 4:
             m_Version = 4;
+            m_4 = IPImpl<4>(data);
             break;
         case 6:
             m_Version = 6;
-            throw std::system_error(
-                std::make_error_code(std::errc::address_family_not_supported),
-                "IPv6 is not supported yet");
+            m_6 = IPImpl<6>(data);
+            break;
         default:
             throw std::system_error(
                 std::make_error_code(std::errc::invalid_argument),
@@ -59,7 +95,20 @@ namespace arpt
 
     IPImpl<4> IP::__GetIPv4() const
     {
+        if (m_Version == 6)
+            throw std::system_error(
+                std::make_error_code(std::errc::operation_not_supported),
+                "Couldn't get IPv4 from IPv6");
         return m_4;
+    }
+
+    IPImpl<6> IP::__GetIPv6() const
+    {
+        if (m_Version == 4)
+            throw std::system_error(
+                std::make_error_code(std::errc::operation_not_supported),
+                "Couldn't get IPv6 from IPv4");
+        return m_6;
     }
 
     uint8_t IP::__GetVersion() const
@@ -67,12 +116,14 @@ namespace arpt
         return m_Version;
     }
 
-    IP::operator const unsigned char*() const
+    IP::operator const uint8_t*() const
     {
         switch (m_Version)
         {
         case 4:
             return m_4;
+        case 6:
+            return m_6;
         default:
             throw std::system_error(
                 std::make_error_code(std::errc::address_family_not_supported),
@@ -86,6 +137,8 @@ namespace arpt
         {
         case 4:
             return m_4.ToString();
+        case 6:
+            return m_6.ToString();
         default:
             throw std::system_error(
                 std::make_error_code(std::errc::address_family_not_supported),
