2fa8b0a2ab05955f327d14b9f5d8bcf7ddae18cf
Sharp0802 <sharp0802.business@gmail.com>
2024-05-05 00:00:12 +0900

    Refactor: Apply copyright to project files

--- inc/log.h
+++ inc/log.h
@@ -1,3 +1,24 @@
+/*
+ * lilac (LILAC)
+ * Copyright (C) Yeong-won Seo  2024
+ *
+ * This file is part of LILAC
+ *
+ * LILAC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3, or (at your option) any later
+ * version.
+ *
+ * LILAC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LILAC; see the file LICENSE.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef LOG_H
 #define LOG_H
 
